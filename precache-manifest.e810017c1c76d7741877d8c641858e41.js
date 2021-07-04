272c1143c0076f79538cd001113b2cf74e425c3f
quinnabrvau <quinnabrvau@gmail.com>
2021-07-04 21:04:14 +0000

    Deploying to gh-pages from @ quinnabrvau/sbb-probabilities@44bb0ec75453b97607d6acb8c5daf2f10e52b39f 🚀

---
+++ precache-manifest.e810017c1c76d7741877d8c641858e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0b285ba5a27bb7fb37b0494e8e6512",
+    "url": "/sbb-probabilities/index.html"
+  },
+  {
+    "revision": "6543bb139cd673ddacc2",
+    "url": "/sbb-probabilities/static/css/2.0b5d44dd.chunk.css"
+  },
+  {
+    "revision": "076117e2ac996d923e32",
+    "url": "/sbb-probabilities/static/css/main.819343d8.chunk.css"
+  },
+  {
+    "revision": "6543bb139cd673ddacc2",
+    "url": "/sbb-probabilities/static/js/2.9f335be9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/sbb-probabilities/static/js/2.9f335be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076117e2ac996d923e32",
+    "url": "/sbb-probabilities/static/js/main.39859fed.chunk.js"
+  },
+  {
+    "revision": "703534df2c01320c5932",
+    "url": "/sbb-probabilities/static/js/runtime-main.213d5d89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bg-probabilities/precache-manifest.5dac20738ae56c8328e9ea5cf0d401cd.js"
+  "/sbb-probabilities/precache-manifest.e810017c1c76d7741877d8c641858e41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bg-probabilities/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sbb-probabilities/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
